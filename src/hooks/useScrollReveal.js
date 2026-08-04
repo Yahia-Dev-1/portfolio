0492fe15dd59172dc790697c57acac68e5cd20c4
Yahia-Dev-1 <yahiapro400@gmail.com>
2026-08-04 17:17:25 +0300

    Fix: Register ScrollTrigger plugin early

--- src/hooks/useScrollReveal.js
+++ src/hooks/useScrollReveal.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
 gsap.registerPlugin(ScrollTrigger);
 
 export const useScrollReveal = () => {
